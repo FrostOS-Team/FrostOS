eea5dcfc8240dcbef895794d33d786bb98d4b286
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-10-12 09:23:16 +0100

    Update frostos.lua

--- frostos.lua
+++ frostos.lua
@@ -5,3 +5,16 @@ os.loadAPI("frostos/apis/sertexapi")
 sertexapi.center(2, "FrostOS")
 sertexapi.center(3, "Installation")
 sertexapi.center(5, "Run on startup?")
+
+write("Y/N>")
+local answer = read()
+local yes = "Y"
+local no = "N"
+if answer == yes then
+  -- Run on startup
+  fs.open("/startup", "w")
+  fs.write("shell.run(\"/frostos/frostos\")")
+  fs.close()
+  sertexapi.center(6, "Startup file created.")
+elseif answer == no then
+  sertexapi.center(6, "Startup file ignored.")
