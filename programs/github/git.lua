e5c0cc93427751c3c4826893a0af2d5e859fa085
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-10-22 13:39:46 +0100

    Update git.lua

--- programs/github/git.lua
+++ programs/github/git.lua
@@ -1,10 +1,31 @@
 -- Github For ComputerCraft
 -- Built for FrostOS
 local github = "api.github.com"
-if fs.exist("/.frostos/apis/git") then
+if not fs.exist("/.frostos/apis/git") then
 print("You don't have the git api! Installing from github...")
 f = fs.open("/.frostos/apis/git", "w")
 fhttp = http.get("https://raw.githubusercontent.com/MultHub/LMNet-OS/master/src/apis/git.lua")
 f.write(fhttp.readAll())
 f.close()
 end
+if not fs.exist("/.frostos/apis/ui") then
+  print("Installing UI api...")
+  f = fs.open("/.frostos/apis/ui", "w")
+  fhttp = http.get("https://raw.githubusercontent.com/MultHub/LMNet-OS/master/src/apis/ui.lua")
+  f.write(fhttp.readAll())
+    f.close()
+  end
+os.loadAPI("/.frostos/apis/ui")
+os.loadAPI("/.frostos/apis/git")
+print("Please login to github before using program")
+write("Username: ")
+user = read()
+write("Password: ")
+pass = read("*")
+-- Login Phase
+
+
+-- table for menu
+-- Options: Clone repo, Create Repo, Edit Repo.
+-- In edit repo: Edit file, Change Name
+
