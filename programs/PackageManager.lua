2d595c4656605462ba84435e509bb66d0b668763
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-11-14 19:11:57 +0000

    Update PackageManager.lua

--- programs/PackageManager.lua
+++ programs/PackageManager.lua
@@ -1,17 +1,18 @@
 local tArgs = {...}
 if #tArgs[1] == "-pkg" then
  if #tArgs[2] == "" then
-  print("No package defined in -pkg flag. Your package installer is invalid or corrupt.")
-  return
+  print("No package defined in -pkg flag. Your package installer is invalid.")
+  return "Invalid"
  end
  --[[ Deal with all packages ]]--
  local pkg == #tArgs[2]
  print("Finding Package: ".. pkg)
- if fs.find(pkg) == true then
+ if fs.exists(pkg) == true then
    print("Package found")
    local pkg2 = textutils.serialize(pkg)
    --[[ Check for valid package]]--
    -- if pkg2 == false then
-    --print(pkg2 .." is a corrupt / invalid package file. Please contact the package author")
+    --print(pkg2 .." is an invalid package file. Please contact the package author")
+    --return "Invalid"
    --end
 end
