8b808427cca59bc37f7e0006d6f6f92409f31093
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-11-14 16:56:32 +0000

    Update PackageManager.lua

--- programs/PackageManager.lua
+++ programs/PackageManager.lua
@@ -3,4 +3,7 @@ if #tArgs[1] == "-pkg" then
  if #tArgs[2] == "" then
   print("No package defined in -pkg flag. Your package installer is invalid or corrupt.")
  end
+--[[ Deal with all packages ]]--
+local pkg == #tArgs[2]
+
 end
