767ff40cc3c1340023e241dc2502e868e31afbb4
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-11-21 16:37:52 +0000

    Update logger.lua

--- apis/logger.lua
+++ apis/logger.lua
@@ -4,3 +4,13 @@ function info(input, file)
  local log = fs.open(file, "a")
  log.write("[INFO]: ".. input)
 end
+
+function warn(input, file)
+ local log = fs.open(file, "a")
+ log.write("[WARN]: ".. input)
+end
+
+function error(input, file)
+ local log = fs.open(file, "a")
+ log.write("[ERROR]: ".. input)
+end
