aa1f1a81a3b8abdbee1bd7ad1d0360be0886d7c7
KingofGamesYami <dirthsj@mcsdonline.org>
2014-11-22 19:37:26 -0600

    Fixed indentation
    I like to use proper indentation.

--- resources/bootHelper.lua
+++ resources/bootHelper.lua
@@ -22,44 +22,44 @@ for _, file in ipairs(FileList) do
 end
 
 function boot.bootLogo()
-local flakes, ok = {}, true
-local maxx, maxy = term.getSize()
-for i = 1, maxx, 3 do
-    flakes[ i + math.random( 0, 2 ) ] = { cx = math.random( 1, maxy ), lx = 0 }
-end
-term.setTextColor( colors.white )
-term.setBackgroundColor( colors.lightBlue )
-term.clear()
-local frost = paintutils.loadImage( "/images/.frost" )
-while ok do
-    term.setTextColor( colors.white )
-    term.setBackgroundColor( colors.lightBlue )
-    for i, flake in pairs( flakes ) do
-        term.setCursorPos( i, flake.lx )
-        term.write( ' ' )
-        term.setCursorPos( i, flake.cx )
-        term.write( i % 2 == flake.cx % 2 and "*" or "%" )
-        flake.lx = flake.cx
-        flake.cx = flake.cx + 1
-        if flake.cx > maxy then
-            flake.cx = 1
-        end
-    end
-    term.setCursorPos( 4, 12 )
-    term.setTextColor( colors.blue )
-    term.write( "Press any key to continue" )
-    paintutils.drawImage( frost, 1, 1 )
-    local t = os.startTimer( 0.2 )
-    while true do
-        local event, param = os.pullEvent()
-        if event == "timer" and param == t then
-            break
-        elseif event == "key" then
-            ok = false
-            break
-        end
-    end
-end
+	local flakes, ok = {}, true
+	local maxx, maxy = term.getSize()
+	for i = 1, maxx, 3 do
+	    flakes[ i + math.random( 0, 2 ) ] = { cx = math.random( 1, maxy ), lx = 0 }
+	end
+	term.setTextColor( colors.white )
+	term.setBackgroundColor( colors.lightBlue )
+	term.clear()
+	local frost = paintutils.loadImage( "/images/.frost" )
+	while ok do
+	    term.setTextColor( colors.white )
+	    term.setBackgroundColor( colors.lightBlue )
+	    for i, flake in pairs( flakes ) do
+	        term.setCursorPos( i, flake.lx )
+	        term.write( ' ' )
+	        term.setCursorPos( i, flake.cx )
+	        term.write( i % 2 == flake.cx % 2 and "*" or "%" )
+	        flake.lx = flake.cx
+	        flake.cx = flake.cx + 1
+	        if flake.cx > maxy then
+	            flake.cx = 1
+	        end
+	    end
+	    term.setCursorPos( 4, 12 )
+	    term.setTextColor( colors.blue )
+	    term.write( "Press any key to continue" )
+	    paintutils.drawImage( frost, 1, 1 )
+	    local t = os.startTimer( 0.2 )
+	    while true do
+	        local event, param = os.pullEvent()
+	        if event == "timer" and param == t then
+	            break
+	        elseif event == "key" then
+	            ok = false
+	            break
+	        end
+	    end
+	end
 end
 
 function boot.loadSettings()
