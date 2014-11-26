a265c4496556fb7a42081832998e2c271e63391d
KingofGamesYami <dirthsj@mcsdonline.org>
2014-11-26 11:12:38 -0600

    Update icemain.lua
    Probably working, but not certain.

--- debug/window/icemain.lua
+++ debug/window/icemain.lua
@@ -1,66 +1,149 @@
-local tApps = {}
+local bigx, bigy = term.getSize() --note: this will need changed.
 
-function drawDock( bColor, tColor )
-    local x, y = term.getSize()
-    term.setBackgroundColor( bColor )
-    term.setTextColor( tColor )
-    for i = 1, y do
-        term.setCursorPos( 1, i )
-        term.write( '   ' )
-    end
-    local y = 3
-    for k, v in pairs( tApps ) do
-        term.setCursorPos( 1, y )
-        term.write( v:getName():sub( 1, 3 ) )
-    end
+function init( win, posx, posy, sizex, sizey, title )
+	local new = {parent = win, posx = posx, posy = posy, sSizex = sizex, sSizey = sizey, x = 1, y = 1, title = title, tColor = colors.white, bColor = colors.black, state = "small", saved = {} }
+	
+	for x = 1, sizex do
+		new.saved[ x ] = {}
+		for y = 1, sizey do
+			new.saved[ x ][ y ] = { bUpdate = false, char = ' ', tColor = colors.white, bColor = colors.black }
+		end
+	end
+	
+	new.isColor = function()
+		return new.parent.isColor()
+	end
+	new.isColour = new.isColor
+	
+	new.setState = function( state )
+		if state ~= "small" and state ~= "large" then
+			error( "Invalid state", 2 )
+		end
+		new.state = state
+	end
+	
+	new.getState = function()
+		return new.state
+	end
+	
+	new.getParent = function()
+		return new.parent
+	end
+	
+	new.getTitle = function()
+		return new.title
+	end
+	
+	new.getSize = function()
+		if new.getState() == "small" then
+			return new.sSizex, new.sSizey
+		else
+			return bigx, bigy
+		end
+	end
+	
+	new.getPosition = function()
+		return new.posx, new.posy
+	end
+	
+	new.getCursorPos = function()
+		return new.x, new.y
+	end
+	
+	new.setCursorPos = function( x, y )
+		new.x, new.y = x, y
+	end
+	
+	new.setTextColor = function( color )
+		new.tColor = color
+	end
+	
+	new.setTextColour = new.setTextColor
+	
+	new.setBackgroundColor = function( color )
+		new.bColor = color
+	end
+	
+	new.setBackgroundColour = new.setBackgroundColor
+	
+	new.write = function( sText, bOverride )
+		local x,y = new.parent.getCursorPos()
+		local px, py = new.getPosition()
+		local tx, ty = new.getCursorPos()
+		for c in sText:gmatch( '.' ) do
+			if new.saved[ tx ] and new.saved[ tx ][ ty ] then
+				new.saved[ tx ][ ty ].char = c
+				new.saved[ tx ][ ty ].tColor, new.saved[ tx ][ ty ].bColor = new.tColor, new.bColor
+			else
+				break
+			end
+			tx = tx + 1
+			new.setCursorPos( tx, ty )
+		end
+	end
+	
+	new.clear = function()
+		local x, y = new.getSize()
+		for i = 1, y do
+			new.setCursorPos( 1, i )
+			new.write( string.rep( " ", x ) )
+		end
+	end
+
+	new.scroll = function( nLines )
+		if nLines ~= 0 then
+			local newsaved, maxx, maxy = {}, new.getSize()
+			for x, t in ipairs( new.saved ) do
+				for y, pixel in ipairs( t ) do
+					local newy = y - nLines
+					if newy < 0 and newy > maxy then
+						newsaved[ x ][ y ] = pixel
+					end
+				end
+			end
+			new.saved = newsaved
+		end
+	end
+	
+	new.render = function()
+		local x,y = new.parent.getCursorPos()
+		local px, py = new.getPosition()
+		local tx, ty = new.getCursorPos()
+		for x, t in ipairs( new.saved ) do
+			for y, pixel in ipairs( t ) do
+				--update pixels
+				new.setTextColor( pixel.tColor )
+				new.setBackgroundColor( pixel.bColor )
+				--start render--
+				new.parent.setCursorPos( px + tx - 1, py + ty - 1 )
+				new.parent.setTextColor( new.tColor )
+				new.parent.setBackgroundColor( new.bColor )
+				new.parent.write( sText )
+				local newx, newy = new.parent.getCursorPos()
+				new.x, new.y = newx - x, newy - y
+				new.parent.setCursorPos( x, y )
+				--end render--
+			end
+		end
+	end
+
+	return new
 end
 
-local app = {
-    getName = function( self )
-        return self.name
-    end,
-    getWindow = function( self )
-        return self.window
-    end,
-    getStatus = function( self )
-        return coroutine.getStatus( self.co )
-    end,
-    resume = function( self, ... )
-        coroutine.resume( self.co, ... )
-        self:getWindow().redraw()
-    end,
-}
-
-function launch( sFile )
-    if not fs.exists( sFile ) then
-        error( "No such file: " .. sFile, 2 )
-    end
-    local file = fs.open( sFile, "r" )
-    local data = file.readAll()
-    file.close()
-    local func, err = loadstring( data )
-    if not func then
-        error( err, 0 )
-    end
-    local new = {
-        name = sFile,
-        window = icewindow.init( term.current(), 3, 1, 20, 10, sFile ),
-        co = coroutine.create( func ),
-    }
-    setmetatable( new, app )
-    tApps[ #tApps + 1 ] = new
-    return new
-end
-
-function run()
-    while true do
-        local event = { os.pullEvent() }
-        for i, app in ipairs( tApps ) do
-            if app:getStatus() == "suspended" then
-                app:resume( unpack( event ) )
-            elseif app:getStatus() == "dead" then
-                table.remove( tApps, i )
-            end
-        end
-    end
+function drawToolBar( app, bColor, tColor )
+	local win = app:getWindow()
+	local x, y = win.getPosition()
+	local sizex, sizey = win.getSize()
+	local parent = win.getParent()
+	parent.setCursorPos( x, y - 1 )
+	parent.setTextColor( tColor )
+	parent.setBackgroundColor( bColor )
+	parent.write( string.rep( " ", sizex ) )
+	parent.setCursorPos( x + math.ceil( (sizex - 3 - #app:getName() ) /2 ) , y - 1 )
+	parent.write( app:getName() )
+	parent.setCursorPos( sizex + x - 3, y - 1 )
+	parent.write( "-" .. ( win.getState() == "small" and "O" or "o" ) )
+	parent.setTextColor( colors.white )
+	parent.setBackgroundColor( colors.red )
+	parent.write( "X" )
 end
