9c3c3ada4a4187adc231563761329a3c6fff0abc
lewislovesgames <lewisminecraft@hotmail.co.uk>
2014-11-30 14:07:19 +0000

    Update crafterClient.lua

--- internet/crafter/crafterClient.lua
+++ internet/crafter/crafterClient.lua
@@ -1,4 +1,4 @@
 --[[ Crafter Client ]]--
 os.loadAPI("../SnowCloud/SCC")
 --[[ Access The SnowCloud To See If Online ]]--
-SCC.SnowCloud:SendMsg("DNR")
+SCC.SnowCloud:SendMsg("DNR") -- A do not respond request. The SendMsg function runs SnowCloud:State() and finds out the state of the snowcloud.
