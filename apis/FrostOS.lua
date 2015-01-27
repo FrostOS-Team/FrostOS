f1b01356ba1ccc5def56919b2c22add907603312
lewislovesgames <lewisminecraft@hotmail.co.uk>
2015-01-27 17:18:25 +0000

    Update FrostOS.lua

--- apis/FrostOS.lua
+++ apis/FrostOS.lua
@@ -1,13 +1,20 @@
 --[[ Used for program developers who need to manipulate FrostOS ]]--
 function runSys(path)
+ --[[--
  if not path == path + ".sys" then
   path = path + ".sys"
  end
+ 
  if not path == "/.frostos/system/" + path then
   path = "/.frostos/system/" + path
  end
+ --]]--
   if not fs.exists(path) then
    error("System File Doesn't Exist!")
  end
  shell.run(path)
+end
+
+function version()
+ return "v1.0 Alpha"
 end
